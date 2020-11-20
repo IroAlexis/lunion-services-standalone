bc24ff51403a6df25ffa1ff4601c9874779f70d7
IroAlexis <iroalexis@outlook.fr>
2020-11-20 23:48:10 +0100

    [lunion-services] Fix minor memory leak

--- src/main.c
+++ src/main.c
@@ -233,6 +233,7 @@ int main(int argc, char* argv[])
 		if(res != CURLE_OK)
 			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 		// Always cleanup
+		curl_slist_free_all(headers);
 		curl_easy_cleanup(curl);
 	}
 	curl_global_cleanup();
